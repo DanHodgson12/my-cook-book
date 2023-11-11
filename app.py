80fd4fee2854e0615e36e6e8449f374dc2a809e5
DanHodgson12 <>
2023-11-11 16:38:56 +0000

    feat: added POST method functionality for register function

--- app.py
+++ app.py
@@ -27,8 +27,28 @@ def get_recipes():
         "recipes.html", recipes=recipes, meal_types=meal_types)
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        new_user = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+
+        mongo.db.users.insert_one(new_user)
+
+        # put the new_user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
+
     return render_template("register.html")
 
 
