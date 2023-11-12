1af57c0f907e948caa061e894a0ce946004f9262
DanHodgson12 <>
2023-11-12 15:01:42 +0000

    feat: added delete functionality to the table rows

--- static/js/script.js
+++ static/js/script.js
@@ -3,10 +3,15 @@ $(document).ready(function () {
     $('.collapsible').collapsible();
     $('select').formSelect();
 
+    $('#ingredients-table tbody tr:first #delete-ingredient').hide();
     $('#add-ingredient').click(function() {
         let duplicateRow = $('#ingredients-table tbody tr:first').clone();
         duplicateRow.find('input, select').val('');
-        duplicateRow.appendTo('#ingredients-table tbody')
+        duplicateRow.find('#delete-ingredient').show();
+        duplicateRow.appendTo('#ingredients-table tbody');
+    });
+    $('#ingredients-table tbody').on('click', '#delete-ingredient', function() {
+        $(this).closest('tr').remove();
     });
 
 });
