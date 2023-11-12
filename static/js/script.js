13323615a993ad191b22a33ca31c54b6cb064e4e
DanHodgson12 <>
2023-11-12 14:51:58 +0000

    feat: added table to form to allow users to add ingredients

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('select').formSelect();
+
+    $('#add-ingredient').click(function() {
+        let duplicateRow = $('#ingredients-table tbody tr:first').clone();
+        duplicateRow.find('input, select').val('');
+        duplicateRow.appendTo('#ingredients-table tbody')
+    });
+
 });
