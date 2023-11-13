26f84e4c4e087cbb1483aa1d918cb31497b77c78
DanHodgson12 <>
2023-11-13 20:48:47 +0000

    feat: add function to increment step numbers for add recipe form when adding steps

--- static/js/script.js
+++ static/js/script.js
@@ -20,11 +20,17 @@ $(document).ready(function () {
         duplicateRow.find('input, select').val('');
         duplicateRow.find('#delete-step').show();
         duplicateRow.appendTo('#method-table tbody');
+        incrementStepNumber()
     });
     $('#method-table tbody').on('click', '#delete-step', function () {
         $(this).closest('tr').remove();
+        incrementStepNumber()
     });
 
-    
+    function incrementStepNumber() {
+        $('#method-table tbody tr').each(function(index, step){
+            $(step).find('#step-number').text(index + 1);
+        });
+    }
 
 });
