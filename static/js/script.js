797ddca1347089d08b06da7bd8b94939f3e58343
DanHodgson12 <>
2023-11-15 19:53:39 +0000

    feat: added customer JS to ensure select element validation on add recipe form

--- static/js/script.js
+++ static/js/script.js
@@ -4,47 +4,32 @@ $(document).ready(function () {
     $('select').formSelect();
     M.textareaAutoResize($('#ingredients'));
 
-    // $('.ingredients-input:first a').hide();
-    // $('#add-ingredient').click(function() {
-    //     let duplicateInput = $('#ingredients-inputs .ingredients-input:first').clone();
-    //     duplicateInput.find('input').val('');
-    //     duplicateInput.find('a').show();
-    //     duplicateInput.appendTo('#ingredients-inputs');
-    // });
-    // $('#ingredients-inputs').on('click', '.delete-ingredient', function() {
-    //     $(this).closest('.ingredients-input').remove();
-    // });
-    
-
-    // // ingredients
-    // $('#ingredients-table tbody tr:first #delete-ingredient').hide();
-    // $('#add-ingredient').click(function() {
-    //     let duplicateRow = $('#ingredients-table tbody tr:first').clone();
-    //     duplicateRow.find('input, select').val('');
-    //     duplicateRow.find('#delete-ingredient').show();
-    //     duplicateRow.appendTo('#ingredients-table tbody');
-    // });
-    // $('#ingredients-table tbody').on('click', '#delete-ingredient', function() {
-    //     $(this).closest('tr').remove();
-    // });
-
-    // // method
-    // $('#method-table tbody tr:first #delete-step').hide();
-    // $('#add-step').click(function () {
-    //     let duplicateRow = $('#method-table tbody tr:first').clone();
-    //     duplicateRow.find('input, select').val('');
-    //     duplicateRow.find('#delete-step').show();
-    //     duplicateRow.appendTo('#method-table tbody');
-    //     incrementStepNumber()
-    // });
-    // $('#method-table tbody').on('click', '#delete-step', function () {
-    //     $(this).closest('tr').remove();
-    //     incrementStepNumber()
-    // });
-    // function incrementStepNumber() {
-    //     $('#method-table tbody tr').each(function(index, step){
-    //         $(step).find('#step-number').text(index + 1);
-    //     });
-    // }
+    validateMaterializeSelect();
+    function validateMaterializeSelect() {
+        let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+        let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+        }
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                    $(this).children("input").css(classValid);
+                }
+            });
+        }).on("click", function () {
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                $(this).parent(".select-wrapper").children("input").css(classValid);
+            } else {
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                        }
+                    }
+                });
+            }
+        });
+    }
 
 });
